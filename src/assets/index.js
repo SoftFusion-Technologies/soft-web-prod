508827c9a4b89eae058e1f9092cfeb7b3d5c336d
Benjamin <goosta19802@gmail.com>
2025-05-15 00:08:39 -0300

    mas mejoras

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,12 +22,7 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
-import andina from "./projects/andina.png";
 import travellive from "./projects/travellive.png";
 import turufitness from './projects/turufitness.png';
 import rubiperfumeria from './projects/rubiperfumeria.png';
@@ -79,11 +74,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  andina,
   travellive,
   turufitness,
   rubiperfumeria,
